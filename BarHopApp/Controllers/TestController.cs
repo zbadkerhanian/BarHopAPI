9ad489771324c1beb57107cd3391e1fb7404de0b
Zareh <zbadkerhanian@gmail.com>
2019-09-07 17:00:29 -0700

    Initial Commit

---
+++ BarHopApp/Controllers/TestController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using BarHopApp.Models;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+
+
+namespace BarHopApp.Controllers
+{
+    public class TestController : ApiController
+    {
+
+        [HttpPost]
+        public string postString()
+        {
+            return "second Post";
+        }
+
+        public Test postGeo(Test data)
+        {
+            return data;
+        }
+
+        public Object postData(Object data)
+        {
+            return data;
+        }
+
+        [HttpGet]
+        public string get()
+        {
+            return "get function";
+        }
+
+        [HttpPut]
+        public string put()
+        {
+            return "put function";
+        }
+
+        [HttpDelete]
+        public string delete()
+        {
+            return "delete function";
+        }
+    }
+}
---
+++ BarHopApp/Models/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BarHopApp.Models
+{
+    public class Test
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Test2
+    {
+        public Test t { get; set; }
+    }
+}
