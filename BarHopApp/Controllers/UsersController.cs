84e287f094338dbbc8029b437b1eb85a8b468e46
Your Name <zbadkerhanian@gmail.com>
2019-11-02 21:45:30 -0700

    Users controller and model

---
+++ BarHopApp/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using BarHopApp.Models;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+
+
+namespace BarHopApp.Controllers
+{
+    public class UsersController : ApiController
+    {
+
+        [HttpPost]
+        public string postString()
+        {
+            return "second Post";
+        }
+
+        public User postUser(User data)
+        {
+            return data;
+        }
+
+        public Object postData(Object data)
+        {
+            return data;
+        }
+
+        [HttpGet]
+        public string get()
+        {
+            return "get function";
+        }
+
+        [HttpPut]
+        public string put()
+        {
+            return "put function";
+        }
+
+        [HttpDelete]
+        public string delete()
+        {
+            return "delete function";
+        }
+    }
+}
---
+++ BarHopApp/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BarHopApp.Models
+{
+    public class User
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+    }
+
+}
